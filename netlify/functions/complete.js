866a94e53047af5f456410fd3a3761dfe0c789b5
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-03-16 17:21:14 +0100

    Update complete.js

--- netlify/functions/complete.js
+++ netlify/functions/complete.js
@@ -1,54 +1,15 @@
-// netlify/functions/complete.js
+const axios=require("axios");
 
-const axios = require("axios");
+exports.handler=async(event)=>{
 
-const PI_API = "https://api.minepi.com/v2/payments";
-const PI_API_KEY = process.env.PI_API_KEY;
+const {paymentId}=JSON.parse(event.body);
 
-exports.handler = async function (event, context) {
+await axios.post(
+"https://api.minepi.com/v2/payments/"+paymentId+"/complete",
+{},
+{headers:{Authorization:`Key ${process.env.PI_API_KEY}`}}
+);
 
-  try {
-
-    const data = JSON.parse(event.body);
-    const paymentId = data.paymentId;
-
-    if (!paymentId) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({
-          error: "Payment ID is required"
-        })
-      };
-    }
-
-    const response = await axios.post(
-      `${PI_API}/${paymentId}/complete`,
-      {},
-      {
-        headers: {
-          Authorization: `Key ${PI_API_KEY}`
-        }
-      }
-    );
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        success: true,
-        data: response.data
-      })
-    };
-
-  } catch (error) {
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify({
-        success: false,
-        error: error.message
-      })
-    };
-
-  }
+return {statusCode:200};
 
 };
