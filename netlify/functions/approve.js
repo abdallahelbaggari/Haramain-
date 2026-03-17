ac20c7c489d0828c639adcaf879757196fb66f3c
abdallahelbaggari <abdallahelbaggari@gmail.com>
2026-03-17 21:25:01 +0100

    Update approve.js

--- netlify/functions/approve.js
+++ netlify/functions/approve.js
@@ -1,27 +1,9 @@
-const axios = require("axios");
+exports.handler = async (event) => {
 
-exports.handler = async function(event, context) {
-  try {
-    const { paymentId, userId } = JSON.parse(event.body);
+  const { paymentId } = JSON.parse(event.body);
 
-    // Example: Call Pi API to approve payment
-    const response = await axios.post("https://api.minepi.com/v2/payments/approve", {
-      paymentId,
-      userId
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        success: true,
-        message: "Payment approved successfully",
-        data: response.data
-      })
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ success: false, error: error.message })
-    };
-  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ approved: true })
+  };
 };
